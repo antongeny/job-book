cb85af9f6b6345b89c10996c7e67054c9372847a
Antongeny <hang.anthony@gmail.com>
2023-07-11 19:01:12 -0400

    set up morgan to display link to our localhost

---
+++ server/app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+const app = express();
+
+app.listen(3000, () => {
+    console.debug('App listening on :3000');
+});
+
+app.use(morgan('tiny'));
+
+// Body parsing middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Set up routes
+app.use("/api", require("./api"));
+
+// Pass back everything else / front-end
+
+app.use("/dist", express.static(path.join(__dirname, "../dist")));
+app.use("/static", express.static(path.join(__dirname, "../static")));
+// app.get("/", (req, res) =>
+//   res.sendFile(path.join(__dirname, "../static/index.html"))
+// );
+app.use("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../static/index.html"));
+});
+
+module.exports = app;
